41d395cd0a2eebbe823f190c49310e8a033ff744
FAchryAP <50170072+FAchryAP@users.noreply.github.com>
2023-04-14 22:34:05 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,17 +144,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
-
+ 
   /**
    * Preloader
    */
